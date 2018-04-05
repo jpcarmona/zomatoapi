8d32992548612d992791f3a84f3336f0c8e8b1b0
jpcarmona <jpcarmona92@gmail.com>
2018-04-05 19:54:32 +0200

    Fin2 Ejercicio1

--- zomatoapi1.py
+++ zomatoapi1.py
@@ -1,26 +1,54 @@
-# 1. Posibilitar buscar restaurantes en otra ciudad.
+# Ejercicio1. Posibilitar buscar restaurantes en otra ciudad.
 
-##-- bash> export key="XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+import os, requests
 
-import os
-from funciones import get_city, get_restaurants
+def get_request(url,key):
+	cabecera={"Accept": "application/json", "user-key": key}
+	r1=requests.get(url,headers=cabecera)
+	return r1
+
+def get_city_details(key,city):
+	url="https://developers.zomato.com/api/v2.1/locations?query="+city
+	r1=get_request(url,key)
+	if r1.status_code == 200:
+		doc=r1.json()
+		if len(doc['location_suggestions']) == 0:
+			return False
+		else:			
+			entity_id=doc['location_suggestions'][0]['entity_id']
+			entity_type=doc['location_suggestions'][0]['entity_type']	
+			city_name=doc['location_suggestions'][0]['city_name']
+			return (entity_id,entity_type,city_name)
+
+def get_restaurants(city_details,key,num):
+	entity_id=city_details[0]
+	entity_type=city_details[1]
+	city_name=city_details[2]
+	url="https://developers.zomato.com/api/v2.1/search?entity_id={}&entity_type={}&start={}".format(entity_id,entity_type,num)
+	r1=get_request(url,key)
+	if r1.status_code == 200:
+		doc = r1.json()		
+		if num == 0:
+			num_res=doc['results_found']
+			print('{} tiene {} restaurantes.\n'.format(city_name,num_res))
+		for restaurant in doc['restaurants']:
+			print('-->',restaurant['restaurant']['name'])
 
 key=os.environ["key"]
 
 city=input('Ciudad para ver sus restaurantes ("0" para salir): ').replace(' ', '%20')
 while city != '0':
-	if not get_city(key,city):
+	if not get_city_details(key,city):
 		print('{} no existe en Zomato'.format(city))
 		city=input('Por favor introduzca una ciudad que exista ("0" para salir): ').replace(' ', '%20')
 		continue
 	else:
-		entity_city=get_city(key,city)
+		city_details=get_city_details(key,city)
 	num=0
-	get_restaurants(entity_city,key,num)
-	opcion=input("\nDesea seguir viendo restaurantes de {} (s/si, n/no)\n".format(entity_city[2]))
+	get_restaurants(city_details,key,num)
+	opcion=input("\nDesea seguir viendo restaurantes de {} (s/si, n/no)\n".format(city_details[2]))
 	while opcion == "s":
 		num+=20
-		get_restaurants(entity_city,key,num)		
+		get_restaurants(city_details,key,num)
 		opcion=input("\nDesea seguir viendo restaurantes de Nueva York (s/si, n/no)\n")
 	city=input('Desea introducir otra ciudad para ver sus restaurantes ("0" para salir): ').replace(' ', '%20')
-
